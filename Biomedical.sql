3a28207b44904d529123ed1443931ddda6718e2f
Aravind Reddy <reddyaravind834@gmail.com>
2024-11-05 02:36:59 -0600

    Biomedical.sql

--- Biomedical.sql
+++ Biomedical.sql
@@ -43,11 +43,11 @@ CREATE TABLE `app` (
 --
 
 INSERT INTO `app` (`id`, `name`, `email`, `date`, `time`, `description`, `regtime`) VALUES
-(16, 'Soumyadip Chowdhury', 'testUser@gmail.com', '6/20/2019', '10:00pm', 'Fever', '2019-06-08 12:22:26'),
-(17, 'Soumyadip Chowdhury', 'testUser@gmail.com', '6/19/2019', '11:00pm', 'Fever', '2019-06-08 12:22:08'),
-(18, 'Soumyadip Chowdhury', 'hello@gmail.com', '6/4/2019', '12:30am', 'Cold', '2019-06-08 13:04:17'),
-(19, 'Soumyadip Chowdhury', 'abc@teamcg.com', '6/5/2019', '12:30am', 'Fever', '2019-06-14 11:40:45'),
-(21, 'Soumyadip Chowdhury', 'testUser@gmail.com', '7/3/2019', '3:30am', 'Fever', '2019-07-03 08:36:17');
+(16, 'aravindreddy', 'testUser@gmail.com', '6/20/2019', '10:00pm', 'Fever', '2019-06-08 12:22:26'),
+(17, 'aravindreddy', 'testUser@gmail.com', '6/19/2019', '11:00pm', 'Fever', '2019-06-08 12:22:08'),
+(18, 'aravindreddy', 'hello@gmail.com', '6/4/2019', '12:30am', 'Cold', '2019-06-08 13:04:17'),
+(19, 'aravindreddy', 'abc@teamcg.com', '6/5/2019', '12:30am', 'Fever', '2019-06-14 11:40:45'),
+(21, 'aravindreddy', 'testUser@gmail.com', '7/3/2019', '3:30am', 'Fever', '2019-07-03 08:36:17');
 
 -- --------------------------------------------------------
 
@@ -91,13 +91,13 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `confirmation_token`, `username`, `enabled`, `first_name`, `gender`, `last_name`, `password`, `authority`, `lastseen`) VALUES
-(1, '36983cce-975b-4a92-bf73-a4f41978e01c', 'soumyadip.yoyo@gmail.com', b'1', 'Soumyadip', 'MALE', 'Chowdhury', 'soumyadip', 'ROLE_ADMIN', 'Thu Aug 22 00:00:56 IST 2019'),
-(2, 'ByAdmin-Panel', 'soumyadip@gmail.com', b'1', 'Soumyadip', 'Male', 'Chowdhury', 'default', 'ROLE_DOCTOR', 'Fri Jun 14 17:11:47 IST 2019'),
-(4, 'ByAdmin-Panel', 'a.yoyo@gmail.com', b'1', 'Soumyadip', 'MALE', 'Chowdhury', 'default', 'ROLE_DOCTOR', 'Wed Jul 03 14:06:52 IST 2019'),
-(6, 'ByAdmin-Panel', 'soumyadip.yoyo@gmail.com', b'1', 'Soumyadip', 'MALE', 'Chowdhury', 'default', 'ROLE_DOCTOR', 'Wed Aug 21 23:59:07 IST 2019'),
-(7, 'ByAdmin-Panel', 'soumydip.cmp@gmail.com', b'1', 'Mr. Soumyadip', 'MALE', 'Chowdhury', 'default', 'ROLE_ADMIN', 'Sat Jun 08 18:25:03 IST 2019'),
-(12, 'a6866ee4-f568-47a9-9a23-2297ec37c293', 'testUser@gmail.com', b'1', 'Soumyadip', 'Male', 'Chowdhury', 'soumyadip', 'ROLE_USER', 'Wed Aug 21 23:57:20 IST 2019'),
-(20, 'ByAdmin-Panel', 's@teamcg.com', b'1', 'Sanket', 'Male', 'Sarkar', 'default', 'ROLE_DOCTOR', 'Fri Jun 14 17:14:51 IST 2019');
+(1, '36983cce-975b-4a92-bf73-a4f41978e01c', 'aravindreddy.yoyo@gmail.com', b'1', 'aravindreddy', 'MALE', 'reddy', 'aravind', 'ROLE_ADMIN', 'Thu Aug 22 00:00:56 IST 2019'),
+(2, 'ByAdmin-Panel', 'aravindreddy@gmail.com', b'1', 'aravind', 'Male', 'reddy', 'default', 'ROLE_DOCTOR', 'Fri Jun 14 17:11:47 IST 2019'),
+(4, 'ByAdmin-Panel', 'a.yoyo@gmail.com', b'1', 'aravind', 'MALE', 'reddy', 'default', 'ROLE_DOCTOR', 'Wed Jul 03 14:06:52 IST 2019'),
+(6, 'ByAdmin-Panel', 'aravindreddy.yoyo@gmail.com', b'1', 'aravind', 'MALE', 'reddy', 'default', 'ROLE_DOCTOR', 'Wed Aug 21 23:59:07 IST 2019'),
+(7, 'ByAdmin-Panel', 'aravindreddy.cmp@gmail.com', b'1', 'Mr. aravind', 'MALE', 'reddy', 'default', 'ROLE_ADMIN', 'Sat Jun 08 18:25:03 IST 2019'),
+(12, 'a6866ee4-f568-47a9-9a23-2297ec37c293', 'testUser@gmail.com', b'1', 'aravind', 'Male', 'reddy', 'aravind', 'ROLE_USER', 'Wed Aug 21 23:57:20 IST 2019'),
+(20, 'ByAdmin-Panel', 's@teamcg.com', b'1', 'Sai', 'Male', 'kiran', 'default', 'ROLE_DOCTOR', 'Fri Jun 14 17:14:51 IST 2019');
 
 --
 -- Indexes for dumped tables
